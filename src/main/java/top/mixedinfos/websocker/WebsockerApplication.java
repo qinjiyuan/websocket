69df28fe531236e9a1542ac8f5fdee6aa37bb746
qinjiyuan <jiyuan.qin@foxmail.com>
2019-12-27 19:42:57 +0800

    Initial commit

---
+++ src/main/java/top/mixedinfos/websocker/WebsockerApplication.java
@@ -0,0 +1,13 @@
+package top.mixedinfos.websocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/mixedinfos/websocker/WebsockerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mixedinfos.websocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
